4a80e5cf00bf5f0114867180d60e3b9e7bc5e1c8
Thore <kurzgedanke@gmail.com>
2017-10-06 13:21:28 +0200

    Added notes class, added menu function

--- pyNoteBook.py
+++ pyNoteBook.py
@@ -14,35 +14,41 @@ class User:
                                  "magic salt".encode('UTF-8'))
         self.c.execute('INSERT into user(username, password) VALUES (?, ?)',
                        (self.username, pwdHash.hexdigest()))
-        self.c.execute('''CREATE TABLE ?(
-                        heading text,
-                        note text
-                        )''', (self.username,))
         self.conn.commit()
-
-    def get_username(self):
-        self.c.execute('SELECT * from user')
-        userData = self.c.fetchall()
-        for data in userData:
-            print(data)
 
     def get_password(self):
         self.c.execute('SELECT password FROM user WHERE username=(?)',
                        (self.username,))
         return self.c.fetchall()
 
-    def create_database(self):
-        self.c.execute('''CREATE TABLE (?)(
-                        heading text,
-                        note text
-                        )''', (self.username,))
-        self.c.commit
-
 
 class Notes:
-    def __init__(self, c, conn):
+    def __init__(self, username, loggedIn, c, conn):
+        self.username = username
+        self.loggedIn = loggedIn
         self.c = c
         self.conn = conn
+
+    def new_note(self):
+        if self.loggedIn:
+            heading = input('Please enter your heading: \n')
+            note = input('Please enter your notes: \n')
+            self.c.execute('''INSERT into note(username, heading, note) VALUES
+                           (?, ?, ?)''', (self.username, heading, note))
+            self.conn.commit()
+        else:
+            print('You are not logged in!')
+
+    def get_notes(self):
+        if self.loggedIn:
+            self.c.execute('SELECT heading, note FROM note where username=(?)',
+                           (self.username, ))
+            print(self.c.fetchall())
+        else:
+            print('You are not logged in!')
+
+    def delete_notes(self):
+        pass
 
 
 def login(c, conn):
@@ -97,6 +103,7 @@ def create_databases(c, conn):
     try:
         c.execute('''CREATE TABLE note(
                 id integer primary key,
+                username text,
                 heading text,
                 note text
                 )''')
@@ -106,15 +113,41 @@ def create_databases(c, conn):
 
     conn.commit()
 
+def list_notes():
+    pass
+
+def menu(c, conn):
+    logInput = input('Welcome to pyNoteBook!\n1. Login\n2. Register')
+    if logInput == 1:
+        loggedIn = login()
+    elif logInput == 2:
+        loggedIn = False
+        register()
+    else:
+        print('Can\'t recognise your input.')
+
+    if loggedIn[1]:
+        note = Notes(loggedIn[0], c, conn)
+        noteInput = input('1. Show your Notes\n2. New Note\3. Delete Note')
+        if noteInput == 1:
+            pass
+        elif noteInput == 2:
+            pass
+        elif noteInput == 3:
+            pass
+        else:
+            print('Can\'t recognise your input.')
+
 
 def main():
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
-
     create_databases(c, conn)
-    register(c, conn)
+    menu(c, conn)
 
-    print('Login: ' + str(login(c, conn)))
+    note = Notes('Thore', c, conn)
+    note.new_note()
+    note.get_notes()
 
     conn.close()
 
